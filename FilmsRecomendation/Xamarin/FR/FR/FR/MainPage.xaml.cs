ec47691943db7dea163d3998deba30b5777e03ca
juliocsoft <juliocto2015@gmail.com>
2020-03-29 10:43:32 -0300

    :construction: Waiting Azure issue

--- FilmsRecomendation/Xamarin/FR/FR/FR/MainPage.xaml.cs
+++ FilmsRecomendation/Xamarin/FR/FR/FR/MainPage.xaml.cs
@@ -2,6 +2,10 @@
 using FR.Models;
 using FR.Services;
 using FR.ViewModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
 using Xamarin.Forms;
 
 namespace FR
@@ -16,12 +20,40 @@ namespace FR
         public MainPage()
         {
             InitializeComponent();
-            _api = ApiService.Instance;
-            _movieViewModel = new MovieViewModel(_api);
-            var movies =_movieViewModel.GetMovies();
-            int a = 1;
+
+            
+            //_api = ApiService.Instance;
+            //_movieViewModel = new MovieViewModel(_api);
+            //var movies =_movieViewModel.GetMovies();
 
             //Page = GetPage();
+        }
+
+        public void Click()
+        {
+        }
+        async void Click(object sender, EventArgs args)
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+
+                // envia a requisição GET
+                var uri = "https://localhost:5001/v1/movies";
+                var result = await client.GetStringAsync(uri);
+                // processa a resposta
+                var posts = JsonConvert.DeserializeObject<Movie>(result);
+                // gera a saida
+                var post = posts.OriginalTitle;
+                label.Text = "Primeiro get:\n\n" + post;
+                }
+                catch (Exception ex)
+                {
+                    label.Text = ex.Message;
+                }
+            }
+            await label.RelRotateTo(360, 1000);
         }
 
         private AppPage GetPage()
