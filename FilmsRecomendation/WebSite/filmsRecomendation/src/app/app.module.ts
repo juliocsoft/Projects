4f01818a9d68a55260af74d599dcac158b074107
juliocsoft <juliocto2015@gmail.com>
2020-03-31 22:42:54 -0300

    :rocket: Add genrepage and film card

--- FilmsRecomendation/WebSite/filmsRecomendation/src/app/app.module.ts
+++ FilmsRecomendation/WebSite/filmsRecomendation/src/app/app.module.ts
@@ -4,16 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/shared/nav/nav.component';
+import { FilmCardComponent } from './components/film-card/film-card.component';
 import { GraphsComponent } from './pages/graphs/graphs.component';
 import { FramePageComponent } from './pages/master/frame-page/frame-page.component';
 import { HighchartsChartModule } from 'highcharts-angular';
+import { GenrePageComponent } from './pages/genre-page/genre-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    GraphsComponent,
-    FramePageComponent    
+    FilmCardComponent,    
+    GraphsComponent,//GraphsPage
+
+    FramePageComponent,
+    GenrePageComponent,
   ],
   imports: [
     BrowserModule,
---
+++ FilmsRecomendation/WebSite/filmsRecomendation/src/app/pages/genre-page/genre-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-genre-page',
+  templateUrl: './genre-page.component.html'
+})
+export class GenrePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
